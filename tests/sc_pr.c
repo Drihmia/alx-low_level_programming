079f83c36cf1e27ab03f5eec24c791d4885bb446
Drihmia <drihmia.redouane@gmail.com>
2023-08-11 14:12:07 +0100

    d

--- tests/sc_pr.c
+++ tests/sc_pr.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
-int age;
-char M[]="Morocco";
+
+/*the main function */
 int main(void)
 {
+
+	/*
+	* the main function
+	*ww
+	*wwdwd
+	*print age
+	*/
+	char M[] = "Morocco";
+	int age;
+
+
+	/*the main function */
 	printf("Enter your age\t");
-	scanf("%d",&age);
-	printf("Your age is %d \t and you are from %s\n",age,M);
-	return 0;
+	scanf("%d ", &age);
+	printf("Your age is %d \t and you are from %s\n, age, M);
+
+	return (0);
 }
