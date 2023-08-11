6d300e9a60438ec447cb88f6f2aaf50eafb6734b
Drihmia <drihmia.redouane@gmail.com>
2023-08-10 17:58:21 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+
+/**
+ * main - integer
+ * Return: 1
+ */
+int main(void)
+{
+	/**
+	 * @message: assigne the sentence provided
+	 */
+	 const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	/**
+	 * write - write the message directly to stderr
+	 */
+	write(STDERR_FILENO, message, sizeof(message) -1);
+	return (1);
+}
