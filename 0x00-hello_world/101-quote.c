fef8a8a05c0e558c018a52e4badc671a85c3cf88
Drihmia <drihmia.redouane@gmail.com>
2023-08-10 18:08:17 -0700

    removed the const keyword and shorted the name of variable

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,11 +10,11 @@ int main(void)
 	/**
 	 * @message: assigne the sentence provided
 	 */
-	 const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	 char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	/**
 	 * write - write the message directly to stderr
 	 */
-	write(STDERR_FILENO, message, sizeof(message) - 1);
+	write(STDERR_FILENO, str, sizeof(str) - 1);
 	return (1);
 }
