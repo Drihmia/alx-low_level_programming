ad3915a8eae5cb9ee76cbedb2e3c4ce25bc056b2
Drihmia <drihmia.redouane@gmail.com>
2023-08-11 20:09:31 +0100

    using else instead of else if stat

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 		printf("Last digit of %d is %d and is grater than 5\n", n, l);
 	else if (l == 0)
 		printf("Last digit is %d is %d and is 0\n", n, l);
-	else if (l < 6)
+	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	return (0);
 }
