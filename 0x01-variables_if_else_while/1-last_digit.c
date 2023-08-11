4863372fdb85cf933b2f23eb9d0498b4a58c9719
Drihmia <drihmia.redouane@gmail.com>
2023-08-11 16:47:38 +0100

    add el if for less than 6 and not 0 instead on else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	
 	l = n%10;
 
 	if (l > 5)
 		printf("Last digit of %d is %d and is grater than 5\n", n, l);
 	else if (l == 0)
 		printf("Last digit is %d is %d and is 0\n", n, l);
-	else
+	else if (l < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	return (0);
 }
