4d5b75c32ee697a7a2dc3c69292870a9923a8dff
Drihmia <drihmia.redouane@gmail.com>
2023-08-15 20:03:14 +0100

    ö

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
 int main(void)
 {
     _putchar('_');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 
 #include <unistd.h>
 
-int _putchar(char c);
-
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
 #endif /* MAIN_H */
