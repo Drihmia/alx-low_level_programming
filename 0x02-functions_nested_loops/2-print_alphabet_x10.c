764d4c9f3f86c5e36e8bc43345d446e2c60b2206
Drihmia <drihmia.redouane@gmail.com>
2023-08-15 14:50:50 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (j = 0; j <= 9; j++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
-	}
 	_putchar('\n');
+	}
 }
