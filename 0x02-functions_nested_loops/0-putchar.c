73adb8e13bf2cfcbb0db7d1515d3b7b7ef30304f
Drihmia <drihmia.redouane@gmail.com>
2023-08-15 20:00:16 +0100

    def appr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
-int _putchar(char);
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
 
-/**
- * main - integer
- * Return: 0
- */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
