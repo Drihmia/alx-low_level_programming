2f1b8479cd9a482f27f599b221763f18af13e2d3
Drihmia <drihmia.redouane@gmail.com>
2023-08-16 23:12:22 +0100

    Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line. The numbers should be separated by comma, followed by a space , . You are allowed to use the standard library. You are not allowed to use any other library (You can’t use GMP etc…). You are not allowed to use long long, malloc, pointers, arrays/tables, or structures. You are not allowed to hard code any Fibonacci number (except for 1 and 2)

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,6 +6,25 @@
  */
 int main(void)
 {
+	long int i, a, b;
 
+	i = 2;
+	a = 1;
+	b = 2;
+
+	while (i <= 48)
+	{
+		printf("%ld", a);
+		printf(", ");
+		printf("%ld", b);
+		if (b != 20365011074)
+		{
+			printf(", ");
+		}
+		a = a + b;
+		b = a + b;
+		i += 2;
+	}
+	printf("\n");
 	return (0);
 }
