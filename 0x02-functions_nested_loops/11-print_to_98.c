4b0425c641d5615289adbf72adff08c04459e60a
Drihmia <drihmia.redouane@gmail.com>
2023-08-16 17:26:27 +0100

    still problem with neative numbers from 1 to 9

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,53 @@
 #include "main.h"
 
 /**
+ * print_digits - print up to 3 digits of a negative or positive numbers
+ * @a: integer
+ * Return: None
+ */
+void print_digits(int a)
+{
+	if ((a >= 0 && a <= 999))
+	{
+		if (a <= 9)
+			_putchar(a + '0');
+		else if (a >= 10 && a <= 99)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		else if (a > 99 && a <= 999)
+		{
+			_putchar(a / 100 + '0');
+			_putchar((a / 10) % 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+	}
+	else	
+	{
+		if (a >= -9)
+		{
+			_putchar('-');
+			_putchar(-(a) + '0');
+		}
+		if (a <= -10 && a >= -99)
+		{
+			_putchar('-');
+			_putchar(-(a / 10) + '0');
+			_putchar(-(a % 10) + '0');
+		}
+		else
+		{
+			_putchar(-(a / 100) + '0');
+			_putchar(-((a / 10) % 10) + '0');
+			_putchar(-(a % 10) + '0');
+		}
+	}
+}
+
+/**
  * print_to_98 - void
+ * @n: integer
  * Return: void
  */
 void print_to_98(int n)
@@ -13,23 +59,13 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			if (i >= 98 && i <= 99)
-			{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			}
+				print_digits(i);
 			else
-			{
-			_putchar(i / 100 + '0');
-			_putchar((i / 10)%10 + '0');
-			_putchar(i % 10 + '0');
-			}
+				print_digits(i);
 			if (i != 98)
-			{
-			_putchar(',');
-			_putchar(' ');
-			}
+				_putchar(',');
+				_putchar(' ');
 		}
-			_putchar('\n');
 	}
 	else
 	{
@@ -38,26 +74,18 @@ void print_to_98(int n)
 			if (j >= 0)
 			{
 				if (j >= 10 && j <= 99)
-				{
-					_putchar(j / 10 + '0');
-					_putchar(j % 10 + '0');
-				}
+					print_digits(j);
 				else
-				_putchar(j + '0');
+					print_digits(j);
 			}
 			else
 			{
-				if (j <= -10 && j >= -99)
-				{
-				_putchar('-');
-				_putchar(-(j / 10) + '0');
-				_putchar(-(j % 10) + '0');
-				}
+				if (j < 0 && j >= -9)
+					print_digits(j);
+				else if (j <= -10 && j >= -99)
+					print_digits(j);
 				else
-				{
-				_putchar('-');
-				_putchar(-(j) + '0');
-				}
+					print_digits(j);
 			}
 			if (j != 98)
 			{
@@ -65,6 +93,6 @@ void print_to_98(int n)
 				_putchar(' ');
 			}
 		}
-			_putchar('\n');
 	}
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/11-print_to_98_back.c
@@ -0,0 +1,84 @@
+#include "main.h"
+
+void print_2_digits(int a)
+{
+	if (a >= 0)
+	{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+	}
+	else
+	{
+				_putchar('-');
+				_putchar(-(j / 10) + '0');
+				_putchar(-(j % 10) + '0');
+	}
+}
+/**
+ * print_to_98 - void
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i >= 98 && i <= 99)
+			{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			}
+			else
+			{
+			_putchar(i / 100 + '0');
+			_putchar((i / 10) % 10 + '0');
+			_putchar(i % 10 + '0');
+			}
+			if (i != 98)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+			_putchar('\n');
+	}
+	else
+	{
+		for (j = n; j <= 98; j++)
+		{
+			if (j >= 0)
+			{
+				if (j >= 10 && j <= 99)
+				{
+					_putchar(j / 10 + '0');
+					_putchar(j % 10 + '0');
+				}
+				else
+				_putchar(j + '0');
+			}
+			else
+			{
+				if (j <= -10 && j >= -99)
+				{
+				_putchar('-');
+				_putchar(-(j / 10) + '0');
+				_putchar(-(j % 10) + '0');
+				}
+				else
+				{
+				_putchar('-');
+				_putchar(-(j) + '0');
+				}
+			}
+			if (j != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+			_putchar('\n');
+	}
+}
