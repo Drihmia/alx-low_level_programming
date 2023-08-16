6369c25dc515048b12858a149d0a8e0170594b2b
Drihmia <drihmia.redouane@gmail.com>
2023-08-15 19:17:54 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 11-main.c 11-print_to_98.c -o 11-98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_to_98 - void
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			if (i != 98)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (j = n; j <= 98; j++)
+		{
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			if (j != 98)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+}
