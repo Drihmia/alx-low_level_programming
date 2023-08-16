1da1ba6cfc49de011cf0c6a408a049546856acdd
Drihmia <drihmia.redouane@gmail.com>
2023-08-16 22:42:13 +0100

    Write a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line. The numbers must be separated by comma, followed by a space ,

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,6 +6,23 @@
  */
 int main(void)
 {
+	long long int i, a, b;
+	i = 2;
+	a = 1;
+	b = 2;
 
+	while (i <= 50)
+	{
+		printf("%lld",a);
+		printf(", ");
+		printf("%lld",b);
+		if (b != 20365011074)
+		{	
+			printf(", ");
+		}
+		a = a + b;
+		b = a + b;
+		i+=2;
+	}
 	return (0);
 }
