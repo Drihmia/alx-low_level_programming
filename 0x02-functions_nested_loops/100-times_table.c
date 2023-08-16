bb31baea055d88d92f78ae699492a18693afb5b5
Drihmia <drihmia.redouane@gmail.com>
2023-08-16 18:26:48 +0100

    created a function for printing comma and spaces for all types of numbers of 1 to 3 digits combination

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,33 @@
 #include "main.h"
 
 /**
+ * print_co_sp - print a comma and spaces before each number a
+ * @a: integer
+ * Return: None
+ */
+void print_co_sp(int a)
+{
+	if (a >= 0 && a <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	else if (a >= 10 && a <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+}
+
+/**
  * print_times_table - void value
  * @n: integer
  * Return: void
@@ -11,7 +38,7 @@ void print_times_table(int n)
 
 	if (n > 0)
 	{
-		if(n < 15)
+		if (n < 15)
 		{
 			for (i = 0; i <= n; i++)
 			{
@@ -22,25 +49,19 @@ void print_times_table(int n)
 					{
 						if (j != 0)
 						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar(' ');
-							_putchar(' ');
+							print_co_sp(k);
 						}
 						_putchar(k + '0');
 					}
 					else if (k >= 10 && k <= 99)
 					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
+						print_co_sp(k);
 						_putchar(k / 10 + '0');
 						_putchar(k % 10 + '0');
 					}
 					else
 					{
-						_putchar(',');
-						_putchar(' ');
+						print_co_sp(k);
 						_putchar(k / 100 + '0');
 						_putchar((k / 10) % 10 + '0');
 						_putchar(k % 10 + '0');
