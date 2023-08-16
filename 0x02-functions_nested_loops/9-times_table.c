f5c9d40cad944d71d72746feecf987f68afa4426
Drihmia <drihmia.redouane@gmail.com>
2023-08-16 15:48:26 +0100

    added two spaced only for 1 digit numbers and 1 space for 2 digit numbers,, after each one of them

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,20 +13,25 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (k <= 0 && k >= 9)
+			if (k >= 0 && k <= 9)
 			{
-				_putchar(k % 10 + '0');
+				_putchar(k + '0');
+				if (j != 9)
+				{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				}
 			}
 			else
 			{
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
-			}
-			if (j != 9)
-			{
+				if (j != 9)
+				{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
