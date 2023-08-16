2a5282bce6bb5b0a3e49f0f8cd4dfc5406ff4e9c
Drihmia <drihmia.redouane@gmail.com>
2023-08-16 18:46:12 +0100

    function for if n greater than 0 and less than 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,6 +28,24 @@ void print_co_sp(int a)
 }
 
 /**
+ * isgreater_0_lower_15 - check If m is greater than 15 or less than 0
+ * @m: integer
+ * Return: 1 is True
+ */
+int isgreater_0_lower_15(int m)
+{
+	if (m >= 0)
+	{
+		if (m < 15)
+		{
+			return (1);
+		}
+		return (0);
+	}
+	return (0);
+}
+
+/**
  * print_times_table - void value
  * @n: integer
  * Return: void
@@ -36,39 +54,36 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n > 0)
+	if (isgreater_0_lower_15(n))
 	{
-		if (n < 15)
+		for (i = 0; i <= n; i++)
 		{
-			for (i = 0; i <= n; i++)
+			for (j = 0; j <= n; j++)
 			{
-				for (j = 0; j <= n; j++)
+				k = i * j;
+				if (k >= 0 && k <= 9)
 				{
-					k = i * j;
-					if (k >= 0 && k <= 9)
-					{
-						if (j != 0)
-						{
-							print_co_sp(k);
-						}
-						_putchar(k + '0');
-					}
-					else if (k >= 10 && k <= 99)
+					if (j != 0)
 					{
 						print_co_sp(k);
-						_putchar(k / 10 + '0');
-						_putchar(k % 10 + '0');
 					}
-					else
-					{
-						print_co_sp(k);
-						_putchar(k / 100 + '0');
-						_putchar((k / 10) % 10 + '0');
-						_putchar(k % 10 + '0');
-					}
+					_putchar(k + '0');
 				}
-				_putchar('\n');
+				else if (k >= 10 && k <= 99)
+				{
+					print_co_sp(k);
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+				else
+				{
+					print_co_sp(k);
+					_putchar(k / 100 + '0');
+					_putchar((k / 10) % 10 + '0');
+					_putchar(k % 10 + '0');
+				}
 			}
+			_putchar('\n');
 		}
 	}
 }
