bb5480437380c30146783e2c31a341c4236d2b4f
Drihmia <drihmia.redouane@gmail.com>
2023-08-16 15:50:49 +0100

    replaced comma and spaces befor numbers not after as it was before

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,23 +15,23 @@ void times_table(void)
 			k = i * j;
 			if (k >= 0 && k <= 9)
 			{
-				_putchar(k + '0');
-				if (j != 9)
+				if (j != 0)
 				{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				}
+				_putchar(k + '0');
 			}
 			else
 			{
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-				if (j != 9)
+				if (j != 0)
 				{
 				_putchar(',');
 				_putchar(' ');
 				}
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
 			}
 		}
 		_putchar('\n');
