b9f0e6dd1d0829e7f0f62aacd2c9d65d6309069a
Drihmia <drihmia.redouane@gmail.com>
2023-08-17 17:25:29 +0100

    remove a trailing space on documentations

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - mulatiplication of two digits
  * @a: first integer.
  * @b: 2nd integer.
- * Return: return the results of a times b 
+ * Return: return the results of a times b
  */
 int mul(int a, int b)
 {
