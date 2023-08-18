ca022e1153728e3d52d9fbd573ee4ea9661d99a4
Drihmia <drihmia.redouane@gmail.com>
2023-08-18 01:30:41 +0100

    greater or equel instead or equal

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-#include <limits.h>
 /**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
+ * main - prints the largest of 3 integers
+ * Return: 0
+ */
 
 int main(void)
 {
 	int a, b, c;
 	int largest;
 
-	a = INT_MIN;
-	b = INT_MIN;
-	c = INT_MIN;
+	a = 3;
+	b = 3;
+	c = 0;
 
 	largest = largest_number(a, b, c);
 
-	printf("%d is the maximum number\n", a);
 	printf("%d is the largest number\n", largest);
 
 	return (0);
