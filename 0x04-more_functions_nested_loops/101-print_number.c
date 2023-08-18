a7e83cafdc3187e7e0b6f8c62c1b767ef877c1c1
Drihmia <drihmia.redouane@gmail.com>
2023-08-18 03:04:57 +0100

    a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,17 +7,65 @@
  */
 void print_number(int n)
 {
-	if (n >= 0 && n <= 9)
-		_putchar(n + '0');
-	else if (n >= 10 && n <= 99)
+	print_digits(n);
+}
+
+/**
+ * print_digits - print up to 3 digits of a negative or positive numbers
+ * @a: integer
+ * Return: None
+ */
+void print_digits(int a)
+{
+	if ((a >= 0 && a <= 999))
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		if (a <= 9)
+			_putchar(a + '0');
+		else if (a >= 10 && a <= 99)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		else if (a > 99 && a <= 999)
+		{
+			_putchar(a / 100 + '0');
+			_putchar((a / 10) % 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		else if (a > 999 && a <= 9999)
+		{
+			_putchar(a/1000 + '0');
+			_putchar((a/100)%10 + '0');
+			_putchar((a/10)%10 + '0');
+			_putchar(a%10 + '0');
+		}
 	}
-	else if (n > 99 && n <= 999)
+	else
 	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
+		if (a >= -9)
+		{
+			_putchar('-');
+			_putchar(-(a % 10) + '0');
+		}
+		else if (a <= -10 && a >= -99)
+		{
+			_putchar('-');
+			_putchar(-(a / 10) + '0');
+			_putchar(-(a % 10) + '0');
+		}
+		else if (a < -99 && a >= -999)
+		{
+			_putchar('-');
+			_putchar(-(a / 100) + '0');
+			_putchar(-((a / 10) % 10) + '0');
+			_putchar(-(a % 10) + '0');
+		}
+		else if (a < -999 && a >= -9999)
+		{
+			_putchar(-(a/1000) + '0');
+			_putchar(-(a/100)%10 + '0');
+			_putchar(-(a/10)%10 + '0');
+			_putchar(-a%10 + '0');
+		}
 	}
 }
