ab7cee7ae1a8f9cbb7c95d27896b90ad718b642b
Drihmia <drihmia.redouane@gmail.com>
2023-08-19 23:38:24 +0100

    very edited version, i have removed print_ 2 to 6 and kept only print_1 and print_m after updated and improved the _pow function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include "main.h"
-void print_4(int n);
-void print_5(int n);
-void print_3(int n);
-void print_2(int n);
+
+int _pow(int m, int n);
+int _len(int n);
+void print_m(int n);
 void print_1(int n);
+void print_number(int n);
 
 
 /**
@@ -13,133 +15,48 @@ void print_1(int n);
  */
 void print_number(int n)
 {
-	print_digits(n);
+	if (_len(n) == 1)
+	{
+		print_1(n);
+	}
+	else if (_len(n) < 11)
+	{
+		print_m(n);
+	}
 }
 
+
 /**
- * print_digits - print up to 3 digits of a negative or positive numbers
- * @a: integer
+ * print_m - print m digits
+ * @n: integer
  * Return: None
  */
-void print_digits(int a)
+void print_m(int n)
 {
-	if (a >= 0)
+	int a, i;
+
+	a = _len(n);
+	if (n >= 0)
 	{
-		if (a <= 9)
-			print_1(a);
-		else if (a >= 10 && a <= 99)
-			print_2(a);
-		else if (a > 99 && a < 1000)
-			print_3(a);
-		else if (a > 999 && a < 10000)
-			print_4(a);
-		else if (a > 9999 && a < 100000)
-			print_5(a);
+		_putchar((n / _pow(10, a - 1)) + '0');
+		for (i = 2; i < a; i++)
+		{
+			_putchar((n / _pow(10, (a - i))) % 10 + '0');
+		}
+		_putchar((n % 10) + '0');
 	}
 	else
 	{
-		if (a >= -9)
-			print_1(a);
-		else if (a <= -10 && a > -100)
-			print_2(a);
-		else if (a < -99 && a > -1000)
-			print_3(a);
-		else if (a < -999 && a > -10000)
-			print_4(a);
-		else if (a < -9999 && a > -100000)
-			print_5(a);
-	}
-}
-
-/**
- * print_3 - print 3 digits
- * @n: integer
- * Return: None
- */
-void print_3(int n)
-{
-	if (n < -99 && n > -1000)
-	{
 		_putchar('-');
-		_putchar(-(n / 100) + '0');
-		_putchar(-((n / 10) % 10) + '0');
-		_putchar(-(n % 10) + '0');
-	}
-	else if (n > 99 && n < 1000)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-}
-/**
- * print_5 - print 5 digits
- * @n: integer
- * Return: None
- */
-void print_5(int n)
-{
-	if (n < -9999 && n > -100000)
-	{
-		_putchar('-');
-		_putchar(-(n / 10000) + '0');
-		_putchar(-(n / 1000) % 10 + '0');
-		_putchar(-(n / 100) % 10 + '0');
-		_putchar(-(n / 10) % 10 + '0');
-		_putchar(-n % 10 + '0');
-	}
-	else if (n > 9999 && n < 100000)
-	{
-		_putchar((n / 10000) + '0');
-		_putchar((n / 1000) % 10 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-}
-/**
- * print_4 - print 4 digits
- * @n: integer
- * Return: None
- */
-void print_4(int n)
-{
-	if (n < -999 && n > -10000)
-	{
-		_putchar('-');
-		_putchar(-(n / 1000) + '0');
-		_putchar(-(n / 100) % 10 + '0');
-		_putchar(-(n / 10) % 10 + '0');
-		_putchar(-n % 10 + '0');
-	}
-	else if (n > 999 && n < 10000)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-}
-
-/**
- * print_2 - print 2 digits
- * @n: integer
- * Return: None
- */
-void print_2(int n)
-{
-	if (n >= 10 && n <= 99)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n <= -10 && n > -100)
-	{
-		_putchar('-');
-		_putchar(-(n / 10) + '0');
+		_putchar(-(n / _pow(10, a - 1)) + '0');
+		for (i = 2; i < a; i++)
+		{
+			_putchar(-(n / _pow(10, a - i)) % 10 + '0');
+		}
 		_putchar(-(n % 10) + '0');
 	}
 }
+
 
 /**
  * print_1 - print 1 digits
@@ -155,4 +72,55 @@ void print_1(int n)
 		_putchar('-');
 		_putchar(-(n % 10) + '0');
 	}
+}
+/**
+ * _len - give a length of un integer.
+ * @n: integer
+ * Return: number of digit in n
+ */
+
+int _len(int n)
+{
+	int i;
+
+	i = 0;
+	if (n < 0)
+	{
+		while (n <= -1)
+		{
+			n /= 10;
+			i++;
+		}
+		return (i);
+	}
+	else
+	{
+		if (n == 0)
+			return (1);
+		while (n >= 1)
+		{
+			n /= 10;
+			i++;
+		}
+		return (i);
+	}
+}
+
+/**
+ * _pow - raised m to the power of n
+ * @m: the base is an integer
+ * @n: the exponent is an integer
+ * Return: m raised to n
+ */
+
+int _pow(int m, int n)
+{
+	int r, i;
+
+	r = 1;
+	for (i = 0; i < n; i++)
+	{
+		r *= m;
+	}
+	return (r);
 }
