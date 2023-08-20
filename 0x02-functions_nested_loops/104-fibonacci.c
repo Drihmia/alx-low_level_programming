c7e2d2a3e243efaa69dc8bbed91e46f00695ab6e
Drihmia <drihmia.redouane@gmail.com>
2023-08-20 05:45:00 +0100

    save my work related to task 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void if_local(int it, long int a, long int b);
+void if_local(int it, int a, int b);
 
 /**
  * main - integer
@@ -7,13 +7,13 @@ void if_local(int it, long int a, long int b);
  */
 int main(void)
 {
-	long int a, b, tempo, it;
+	int a, b, tempo, it;
 
 	a = 1;
 	b = 2;
 	it = 2;
 
-	printf("%ld, %ld", a, b);
+	printf("%d, %d", a, b);
 	while (it < 98)
 	{
 		it += 1;
@@ -22,59 +22,94 @@ int main(void)
 			tempo = a;
 			a = b;
 			b = tempo + a;
-			printf(", %ld", b);
+			printf(", %d", b);
 		}
-		else
+		else if (it == 92)
 		{
 			if_local(it, a, b);
 		}
+		if (it >= 92)
+		{
+			else if (it > 92 && it < 94)
+			{
 	}
 	printf("\n");
 	return (0);
 }
 
 /**
- * if_local - IFs to hundle different cases from 92 up to 98 Fibonacci numbers.
+ * print_92 - print th fibonacci numbers N 92
  * i used same iteration number above to save time.
  * @it: iteration digit.
- * @a: 1st number.
- * @b: 2nd number.
- * Return: None
+ * @a: 1st number is integer.
+ * @b: 2nd number is an integer.
+ * @c: integer for options
+ * Return: b_1 if c = 1 and b_2 if c = 2.
  */
-void if_local(int it, long int a, long int b)
-{
-	int tempo_1, tempo_2, a_1, a_2, b_1, b_2, b_2_1, b_2_2;
 
-	if (it == 92)
-	{
-		tempo_1 = (a / 10000000000);
-		tempo_2 = (a % 10000000000);
-		a_1 = (b / 10000000000);
-		a_2 = (b % 10000000000);
-		b_1 = tempo_1 + a_1;
-		b_2 = tempo_2 + a_2;
-		printf(", %ld%ld", b_1, b_2);
-	}
-	else if (it > 92 && it < 94)
-	{
-		tempo_1 = a_1;
-		tempo_2 = a_2;
-		a_1 = b_1;
-		a_2 = b_2;
-		b_1 = tempo_1 + a_1;
-		b_2 = tempo_2 + a_2;
-		printf(", %ld%ld", b_1, b_2);
-	}
-	else
-	{
-		tempo_1 = a_1;
-		tempo_2 = a_2;
-		a_1 = b_1;
-		a_2 = b_2;
-		b_1 = tempo_1 + a_1;
-		b_2 = tempo_2 + a_2;
-		b_2_1 = b_2 % 10000000000;
-		b_2_2 = b_1 + b_2 / 10000000000;
-		printf(", %ld%ld", b_2_2, b_2_1);
-	}
+int print_92(int a, int b, int it, int c)
+{
+	int tempo_1, tempo_2, a_1, a_2, b_1, b_2;
+	tempo_1 = (a / 10000000000);
+	tempo_2 = (a % 10000000000);
+	a_1 = (b / 10000000000);
+	a_2 = (b % 10000000000);
+	b_1 = tempo_1 + a_1;
+	b_2 = tempo_2 + a_2;
+
+	printf(", %d%d", b_1, b_2);
+	if (c == 1)
+		return (b_1);
+	if (c == 2)
+		return (b_2);
+}
+
+/**
+ * print_93 - print th fibonacci numbers N 93
+ * i used same iteration number above to save time.
+ * @it: iteration digit.
+ * @b_1: 1st number is integer.
+ * @b_2: 2nd number is an integer.
+ * @c: integer for options
+ * Return: b_1 if c = 1 and b_2 if c = 2.
+ */
+
+void print_93(int b_1, int b_2, int it, int c)
+{
+	int tempo_1, tempo_2, a_1, a_2, b_2_1, b_2_2;
+
+	a_1 = b_1;
+	a_2 = b_2;
+	b_1 = tempo_1 + a_1;
+	b_2 = tempo_2 + a_2;
+
+	printf(", %d%d", b_1, b_2);
+	if (c == 1)
+		return (b_1);
+	if (c == 2)
+		return (b_2);
+}
+
+
+
+
+}
+else
+{
+	b_1 = tempo_1 + a_1;
+	b_2 = tempo_2 + a_2;
+	a_1 = b_1;
+	a_2 = b_2;
+	tempo_1 = a_1;
+	tempo_2 = a_2;
+	a_1 = b_1;
+	a_2 = b_2;
+	b_1 = tempo_1 + a_1;
+	b_2 = tempo_2 + a_2;
+	b_2_1 = b_2 % 10000000000;
+	b_2_2 = b_1 + b_2 / 10000000000;
+
+	printf(", %d%d", b_2_2, b_2_1);
+}
+}
 }
