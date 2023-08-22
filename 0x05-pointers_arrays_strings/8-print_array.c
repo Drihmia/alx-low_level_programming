fd40ccfbdb7af0877720383d237ad4da2f6ec424
Drihmia <drihmia.redouane@gmail.com>
2023-08-22 12:49:56 +0100

    eliminate de lenght function and restricted the case for n greater than 0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-int arrlen(int *m);
 /**
  * print_array -  prints n elements of an array of integers
  * @a: integer
@@ -14,10 +13,7 @@ void print_array(int *a, int n)
 	int i, *ptr;
 
 	ptr = a;
-
-	printf("sizeof array is %d\n", arrlen(a));
-	printf("sizeof *a is %lu\n", sizeof(ptr));
-	if ( n == ((sizeof(*a) + 1) / sizeof(a[0])))
+	if ( n > 0)
 	{
 		printf("%d", *a);
 		for (i = 1; i < n; i++)
@@ -26,22 +22,5 @@ void print_array(int *a, int n)
 		}
 	}
 	printf("\n");
-}
-/**
- * arrlen - determine the lenght of a string.
- * @m: integer
- * Return: return the lenght of the given string s.
- */
-int arrlen(int *m)
-{
-	int i;
-
-	i = 0;
-	while (*m != '\0')
-	{
-		m++;
-		i++;
-	}
-	return (i);
 }
 
