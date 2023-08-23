bfdf2b8976e8f75282d8751e2ef1bd03f49f3718
Drihmia <drihmia.redouane@gmail.com>
2023-08-23 18:09:10 +0100

    used pointers syntax only

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -12,11 +12,11 @@ int main(void)
 	char s2[] = "World!\n";
 	char *ptr;
 
-	printf("--- s1 --- %s\n", s1);
-	printf("--- s2 --- %s", s2);
+	printf("%s\n", s1);
+	printf("%s", s2);
 	ptr = _strcat(s1, s2);
-	printf("--- s1 --- %s\n", s1);
-	printf("--- s2 --- %s", s2);
-	printf("--- pt --- %s", ptr);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
 	return (0);
 }
--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,12 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	while (dest[j] != '\0')
+	while (*(dest + j) != '\0')
 	{
 		j++;
 	}
 
-	while (src[i] != '\0')
+	while (*(src + i) != '\0')
 	{
 		*(dest + j) = *(src + i);
 		i++;
