d73f9b5a51d5344a9d75e2415af0466647b3d6da
Drihmia <drihmia.redouane@gmail.com>
2023-08-24 01:02:50 +0100

    if two separators are sequence, the sep iterator inisalize by zeroo

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,14 +12,15 @@ char *cap_string(char *c)
 
 	while (c[i] != '\0')
 	{
-		if ((c[i] >= 97 && c[i] <= 122) && sep == 1)
+		if (sep == 1)
 		{
-			c[i] = (c[i] - 'a' + 'A');
 			sep = 0;
+			if((c[i] >= 97 && c[i] <= 122))
+			{
+				c[i] = (c[i] - 'a' + 'A');
+				i++;
+			}
 		}
-		else if ((c[i] >= 65 && c[i] <= 92) && sep == 1)
-			sep = 0;
-		i++;
 		if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n' || c[i] == ',')
 		{
 			sep = 1;
@@ -40,6 +41,8 @@ char *cap_string(char *c)
 			sep = 1;
 			i++;
 		}
+		else
+			i++;
 	}
 	return (c);
 }
