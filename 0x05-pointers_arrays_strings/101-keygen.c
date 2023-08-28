e2add50acce27f5b04ea884d27372225d7aadf72
Drihmia <drihmia.redouane@gmail.com>
2023-08-28 03:25:39 +0100

    s

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,17 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main - a program that generates random valid passwords
- * for the program 101-crackme
- * Return: 0 
- */
-int main(void)
-{
-	int n;
+/* Function Prototypes */
+int* generateAllPossibilities(void);
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+int main(void) {
+    srand(time(NULL)); // Initialize random seed using the current time
+
+    int* possibilities = generateAllPossibilities();
+
+    // Use the possibilities array as needed
+
+    free(possibilities); // Remember to free the allocated memory
+
+    return 0;
 }
+
+/**
+ * generateAllPossibilities - Generates all possible 4-digit numbers and returns them as an array.
+ * Return: Pointer to the array of possible 4-digit numbers.
+ */
+int* generateAllPossibilities(void) {
+    int* possibilities = (int*)malloc(10000 * sizeof(int)); // Allocate memory for the array
+
+    if (possibilities == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < 10000; ++i) {
+        possibilities[i] = i; // Store the current 4-digit number in the array
+    }
+
+    return possibilities;
+}
+
