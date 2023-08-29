c64b15329efb6cfd8c8227466e13858f84ca2ca7
Drihmia <drihmia.redouane@gmail.com>
2023-08-29 01:24:19 +0100

    fixed some betty issue

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix of integers.
+ * print_diagsums - prints the sum of the two diagonals of a
+ * square matrix of integers.
  * @a: integer
  * @size: integer
  * Return: None
@@ -10,6 +11,7 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j, sum_1 = 0, sum_2 = 0;
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
