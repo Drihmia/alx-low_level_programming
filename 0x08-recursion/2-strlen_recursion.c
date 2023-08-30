2a623bc6e4a08be07c5e26fbd5de2a33659a5fa0
Drihmia <drihmia.redouane@gmail.com>
2023-08-30 20:46:59 +0100

    fixed a new betty issue afte last modification

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s)
 {
 	if (*(s + 1) == '\0')
 		return (1);
-	else if ( *s == '\0')
+	else if (*s == '\0')
 		return (0);
 	return ((((s + 1)) - (s)) + _strlen_recursion(s + 1));
 }
