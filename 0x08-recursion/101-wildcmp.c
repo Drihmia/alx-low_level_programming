02d958dc5e74a05b51fce77101ffa989898d86a4
Drihmia <drihmia.redouane@gmail.com>
2023-08-31 03:21:01 +0100

    saving my work

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings, second string myy contain *.
+ * @s1: pointer to string 1.
+ * @s2: pointer to string 2.
+ * Return: return 1 if 2 string can be considered identical or 0 if not.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	else if (*s1 != *s2)
+		return (wildcmp(s1 + 1, s2));
+	return (wildcmp(s1 + 1, s2 + 1));
+}
