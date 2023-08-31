d637aefd43dadb96af3c3b4a015054be930bcc46
Drihmia <drihmia.redouane@gmail.com>
2023-08-31 03:46:18 +0100

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,10 +9,12 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' || *s2 == '\0')
+	if (*s1 == '\0')
 		return (0);
-	if (*s2 == '*')
+	if (*s2 == '*' && *(s2 + 1) != '\0')
 		return (wildcmp(s1, s2 + 1));
+	else if (*s1 != *s2 && *s2 != '*')
+		return (wildcmp(s1 + 1, s2));
 	else if (*s1 != *s2)
 		return (wildcmp(s1 + 1, s2));
 	return (wildcmp(s1 + 1, s2 + 1));
