4e71b33cb7ea8011b7859bb231aeef5ad3bfce58
Drihmia <drihmia.redouane@gmail.com>
2023-09-01 23:47:11 +0100

    restarting my work

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -28,43 +28,4 @@ int main(int argc, char **argv)
 	}
 }
 
-/**
- * change_recursion - give number of coins to make change for an amount
- * of money.
- * @money: amount money.
- * @N_coins: nomber of coins needed for change.
- * Return: number of coins to make change.
- */
 
-int change_recursion(int money, int N_coins)
-{
-	if (money >= 25)
-	{
-		N_coins += money / 25;
-		money = money - 25 * N_coins;
-		return (change_recursion(money, N_coins));
-	}
-	else if (money >= 10)
-	{
-		N_coins += money / 10;
-		money = money - 10 * N_coins;
-		return (change_recursion(money, N_coins));
-	}
-	else if (money >= 5)
-	{
-		N_coins += money / 5;
-		money = money - 5 * N_coins;
-		return (change_recursion(money, N_coins));
-	}
-	else if (money >= 2)
-	{
-		N_coins += money / 2;
-		money = money - 2 * N_coins;
-		return (change_recursion(money, N_coins));
-	}
-	else if (money == 1)
-	{
-		return (change_recursion(money - 1, N_coins + 1));
-	}
-	return (N_coins);
-}
