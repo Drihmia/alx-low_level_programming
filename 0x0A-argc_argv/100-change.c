b17456711efcb628361ced7af8d32fffb9eda8ce
Drihmia <drihmia.redouane@gmail.com>
2023-09-02 01:22:59 +0100

    added other function to help

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+int change(int money);
 
 
 /**
@@ -11,7 +12,7 @@
  */
 int main(int argc, char **argv)
 {
-	int money, coins = 0;
+	int money;
 
 	if (argc == 2)
 	{
@@ -22,35 +23,7 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-			while (money != 0)
-			{
-				if (money >= 25)
-				{
-					coins += money / 25;
-					money = money - 25 * coins;
-				}
-				else if (money < 25 && money >= 10)
-				{
-					coins += money / 10;
-					money = money - 10 * coins;
-				}
-				else if (money < 10 && money >= 5)
-				{
-					coins += money / 5;
-					money = money - 5 * coins;
-				}
-				else if (money < 5 && money >= 2)
-				{
-					coins += money / 2;
-					money = money - 2 * coins;
-				}
-				else
-				{
-					coins += 1;
-					money = 0;
-				}
-			}
-			printf("%d\n", coins);
+			printf("%d\n", change(money));
 		}
 	}
 	else
@@ -59,4 +32,46 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	return (0);
+}
+
+/**
+ * change - give number of coins to make change for an amount
+ * of money.
+ * @money: amount money.
+ * Return: number of coins to make change.
+ */
+
+int change(int money)
+{
+	int coins = 0;
+
+	while (money != 0)
+	{
+		if (money >= 25)
+		{
+			coins += money / 25;
+			money = money - 25 * coins;
+		}
+		else if (money < 25 && money >= 10)
+		{
+			coins += money / 10;
+			money = money - 10 * coins;
+		}
+		else if (money < 10 && money >= 5)
+		{
+			coins += money / 5;
+			money = money - 5 * coins;
+		}
+		else if (money < 5 && money >= 2)
+		{
+			coins += money / 2;
+			money = money - 2 * coins;
+		}
+		else
+		{
+			coins += 1;
+			money = 0;
+		}
+	}
+	return (coins);
 }
