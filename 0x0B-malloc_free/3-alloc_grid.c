a809edd86eea1a9dae142b38d59d9fc77cc8f136
Drihmia <drihmia.redouane@gmail.com>
2023-09-05 00:56:00 +0100

    update 2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 		ar_2[i] = malloc(width * sizeof(int));
 		if (ar_2[i] == NULL)
 		{
-			for (j = i; j >= 0; j--)
+			for (j = 0; j < height; j++)
 				free(ar_2[j]);
 			return (NULL);
 		}
