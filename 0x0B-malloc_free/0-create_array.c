e9e82b2a6ba252c5309de90d70043f1af947f688
Drihmia <drihmia.redouane@gmail.com>
2023-09-04 17:29:50 +0100

    took in consideration if c is empty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *ar;
 
-	if (size == 0)
+	if (size == 0 || c == '\0')
 		return (NULL);
 
 	ar = malloc((size + 1) * sizeof(char));
