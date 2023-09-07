19ba42e87137058e5038cb89e01244921819f0b0
Drihmia <drihmia.redouane@gmail.com>
2023-09-07 17:33:39 +0100

    added one byte in malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		exit(0);
 
-	ar = malloc(size * nmemb);
+	ar = malloc(size * nmemb + 1);
 
 	if (ar == NULL)
 	{
