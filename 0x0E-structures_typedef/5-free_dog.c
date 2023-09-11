c20c9b9f6e04322f3015557c68e3bcc039d7dcba
Drihmia <drihmia.redouane@gmail.com>
2023-09-11 23:27:25 +0100

    move my work to sandbox

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs.
+ * @d: pointer to struct dog.
+ * Return: Void.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+
