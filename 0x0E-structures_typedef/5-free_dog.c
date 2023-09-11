b172eb18a983dc40e0581db91cfbe5ac232ec280
Drihmia <drihmia.redouane@gmail.com>
2023-09-12 00:38:27 +0100

    used big if for struct name 1st the n inside that if i used for members

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,11 +7,13 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d->name != NULL)
-		free(d->name);
-	if (d->owner != NULL)
-		free(d->owner);
 	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
 		free(d);
+	}
 }
 
