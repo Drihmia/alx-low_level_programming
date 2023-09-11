6ec33ebfdb305266a651f01ea4ea4f6652c0b791
Drihmia <drihmia.redouane@gmail.com>
2023-09-11 21:06:03 +0100

    redmoved the handle because the function already return nothin and added stdlib in the head of the file directly

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct dog.
@@ -8,11 +9,10 @@
  * @owner: dog's owner.
  * Return: None
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
