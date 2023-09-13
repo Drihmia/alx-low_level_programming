b3c211e79e55ef3dfb825c5c551a863c935784cb
Drihmia <drihmia.redouane@gmail.com>
2023-09-13 19:49:28 +0100

    source file for  a function that prints a name. and a header file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @f: function pointer that take str.
+ * @name: name to be printed.
+ * Return: None.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
