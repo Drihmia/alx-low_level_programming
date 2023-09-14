747003fb326d7eaf11cc20c0a4f395e55a4acfde
Drihmia <drihmia.redouane@gmail.com>
2023-09-14 18:44:24 +0100

    fixed some edge cases issues

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ls;
 	unsigned int i;
 
-	if (separator != NULL && n != 0)
+	va_start(ls, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(ls, n);
-		for (i = 0; i < n; i++)
-		{
-			if (i != 0)
-				printf("%s", separator);
-			printf("%d", va_arg(ls, int));
-		}
-		va_end(ls);
-		printf("\n");
-		exit(EXIT_SUCCESS);
+		if (i != 0 || separator != NULL)
+			printf("%s", separator);
+		printf("%d", va_arg(ls, int));
 	}
+	va_end(ls);
+	printf("\n");
+	exit(EXIT_SUCCESS);
 }
