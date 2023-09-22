0eb57aec728ad2a502e4025b7f97662c60a2fc05
Drihmia <drihmia.redouane@gmail.com>
2023-09-22 01:27:43 +0100

    added the prototype of my function into header file instead in the source file itself

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+
+/**
+ * b_main - Function to execute b main
+ */
+void b_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
