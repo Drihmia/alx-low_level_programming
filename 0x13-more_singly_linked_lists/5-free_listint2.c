2838be4f1916943e99c8b20a7c4828578ac7047a
Drihmia <drihmia.redouane@gmail.com>
2023-09-25 20:25:17 +0100

    added null to end

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,11 @@ void free_listint2(listint_t **head)
 
 	if (!(*head) || !head)
 		return;
-	*head = NULL;
 	while (courant)
 	{
 		next = courant->next;
 		free(courant);
 		courant = next;
 	}
+	*head = NULL;
 }
