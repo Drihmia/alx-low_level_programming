610173498b3958c3cd71e72c765a38b45eee664a
Drihmia <drihmia.redouane@gmail.com>
2023-09-26 21:01:01 +0100

    included every line inside a if that check if the head is not null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next;
 
-	while (*head)
+	if ((*head) != NULL)
 	{
-		next = (*head)->next;
-		free(*head);
-		*head = next;
+		while (*head)
+		{
+			next = (*head)->next;
+			free(*head);
+			*head = next;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
