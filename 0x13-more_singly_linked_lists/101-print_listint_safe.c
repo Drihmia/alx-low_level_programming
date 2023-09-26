cba4de599b82f20684bd7057e41d7959e3f07b9f
Drihmia <drihmia.redouane@gmail.com>
2023-09-27 00:31:10 +0100

    used floyd tech of two pointer of diff speed

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,11 +8,24 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0;
+	const listint_t *cur = head, *faster = head;
 
-	while (head)
+	if (!head)
+		exit(98);
+
+	while (faster && (faster->next))
 	{
-		printf("%d\n", head->n);
-		head = head->next;
+		printf("[%p] %d\n", (void *)cur, cur->n);
+		cur = cur->next;
+		faster = faster->next->next;
+		if (faster == cur)
+			exit(98);
+	}
+	i++;
+	while (cur)
+	{
+		printf("[%p] %d\n", (void *)cur, cur->n);
+		cur = cur->next;
 		i++;
 	}
 	return (i);
