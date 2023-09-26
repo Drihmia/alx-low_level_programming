81567fcf1cab41f175a97abb38073d666e8861e0
Drihmia <drihmia.redouane@gmail.com>
2023-09-26 21:24:31 +0100

    added null to new in the allcoation

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
+	new->next = NULL;
 	if (idx == 0)
 	{
 		hold = add_nodeint(&(*head), n);
@@ -37,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		courant = get_nodeint_at_index(*head, idx - 1);
 		courant->next = new;
-new->next = NULL;
+		new->next = NULL;
 		return (new);
 	}
 	else
