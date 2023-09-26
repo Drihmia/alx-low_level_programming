05c6a8f76fbae8c19a1b5416363bbf5bbb49d598
Drihmia <drihmia.redouane@gmail.com>
2023-09-26 21:36:19 +0100

    e

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		courant = get_nodeint_at_index(*head, idx - 1);
 		courant->next = new;
-		new->next = NULL;
 		return (new);
 	}
 	else
