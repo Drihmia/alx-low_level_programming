f29100582ce27ae8eb62ee8b5d538184bf47c0f5
Drihmia <drihmia.redouane@gmail.com>
2023-09-26 12:04:55 +0100

    added case where the index is greater than the size of the list -1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,9 +12,14 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *courant, *prev;
+	size_t len ;
 
 	if (!(*head))
 		return (-1);
+	len = listint_len(*head) - 1;
+	if (index > (unsigned int)len)
+		return (-1);
+
 	prev = get_nodeint_at_index(*head, index - 1);
 	courant = get_nodeint_at_index(*head, index);
 	if (!prev)
@@ -54,4 +59,18 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		i++;
 	}
 	return (NULL);
+}
+
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to a header of signly linked list.
+ * Return: the number of elements.
+ */
+size_t listint_len(const listint_t *h)
+{
+	if (!h)
+		return (0);
+	else
+		return (1 + listint_len(h->next));
 }
