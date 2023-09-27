f5d925c166a24326d35da0795f0030dc24fee79e
Drihmia <drihmia.redouane@gmail.com>
2023-09-27 22:17:34 +0100

    added a case if the pointer to pointer head is null , if not head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next;
 
-	if ((*head) == NULL)
+	if (!(*head) && !head)
 		return;
 	while (*head)
 	{
@@ -17,4 +17,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = next;
 	}
+	(*head) = NULL;
 }
