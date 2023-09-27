385c427c0288cf149b94d1cd05189fb56d92ed56
Drihmia <drihmia.redouane@gmail.com>
2023-09-27 22:42:50 +0100

    learned a valuable lessong, dispite the fact that i had added the if condition for a NULL pointer but i was derefrencing the pointer in the variable declaration before condtion

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *next, *current = *head;
+	listint_t *next, *current;
 
-	if ((*head) == NULL || head == NULL)
+	if ((head) == NULL || (*head) == NULL)
 		return;
+	current = *head;
 	while (current)
 	{
 		next = (current)->next;
