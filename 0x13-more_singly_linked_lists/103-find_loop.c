fbfbe2587330c1dad116e8e83bbbeab8d9ade185
Drihmia <drihmia.redouane@gmail.com>
2023-09-28 05:19:35 +0100

    9 hours of work for one checker

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,4 +1,8 @@
 #include "lists.h"
+listint_t *node_loop(listint_t *h);
+int there_loop(listint_t *head);
+
+
 
 /**
  * find_listint_loop - finds the loop in a linked list.
@@ -8,17 +12,65 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	const listint_t *cur = head, *faster = head;
+	listint_t *red;
+
+	if (!head || !(head->next))
+		return (NULL);
+	if (there_loop(head))
+	{
+		red = node_loop(head);
+		return (red);
+
+	}
+	return (NULL);
+}
+/**
+ * there_loop - check if there is a loop.
+ * @head: pointer node.
+ * Return: 1 i found, 0 if not.
+ */
+int there_loop(listint_t *head)
+{
+	listint_t *cur, *faster;
 
 	if (!head)
-		return (NULL);
-
+		return (0);
+	cur = head;
+	faster = head;
 	while (cur && faster && (faster->next))
 	{
 		cur = cur->next;
 		faster = faster->next->next;
 		if (faster == cur)
-			return ((void *)cur);
+			return (1);
+	}
+	return (0);
+}
+
+
+/**
+ * node_loop - check is the node entred is node loop.
+ * @h: pointer node.
+ * Return: 1 i found, 0 if not.
+ */
+listint_t *node_loop(listint_t *h)
+{
+	listint_t *start, *cur;
+
+	if (!h)
+		return (NULL);
+	start = h->next;
+	cur = (h)->next->next;
+
+	while (start)
+	{
+		if ((((char *)(start) - (char *)(cur)) != 16))
+		{
+			return (cur);
+		}
+		start = (start)->next;
+		cur = cur->next;
 	}
 	return (NULL);
 }
+
