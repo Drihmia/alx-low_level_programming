507ea34a80759b5adf13dcd1d6e05c909ad3cdf0
Drihmia <drihmia.redouane@gmail.com>
2023-09-28 22:14:57 +0100

    edited the if condition

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int ret;
 
-	if (index < 0 || n < 0)
+	if ((n >> (index - 0) ==  0))
 		return (-1);
 	ret = ((n >> (index - 0)) & 1);
 	return (ret);
