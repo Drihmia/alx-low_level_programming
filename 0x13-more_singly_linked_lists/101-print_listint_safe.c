c48fd71ebf19fa95e9848bac586874ae8bda4131
Drihmia <drihmia.redouane@gmail.com>
2023-09-29 14:35:48 +0100

    added the function from task 14 inside the source code

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -65,3 +65,38 @@ int there_loop(listint_t *head)
 	}
 	return (0);
 }
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: pointer to a header of signly linked list.
+ * Return: The address of the node where the loop starts,
+ * or NULL if there is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *cur, *faster;
+
+	if (!head || !(head->next))
+		return (NULL);
+
+	cur = head;
+	faster = head;
+	while (cur && faster && (faster->next))
+	{
+		cur = cur->next;
+		faster = faster->next->next;
+		if (faster == cur)
+		{
+			cur = head->next;
+			faster = head->next->next;
+			while (cur)
+			{
+				if (((char *)cur - (char *)faster) != 16)
+					return (faster);
+				cur = cur->next;
+				faster = faster->next;
+			}
+		}
+	}
+	return (NULL);
+}
