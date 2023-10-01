413d8a7854b7c90d0cb3fafc4259c074b9b44355
Drihmia <drihmia.redouane@gmail.com>
2023-10-01 11:50:40 +0100

    t

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,7 +10,7 @@ size_t free_listint_safe(listint_t **h)
 	listint_t *next;
 	size_t i = 0;
 
-	if (!(h) && !*h)
+	if (!(h) || !*h)
 		return (0);
 	while (*h)
 	{
@@ -21,4 +21,37 @@ size_t free_listint_safe(listint_t **h)
 	}
 	(*h) = NULL;
 	return (i);
+}
+
+/**
+ * _find_listint_loop - finds the loop in a linked list.
+ * @head: pointer to a header of signly linked list.
+ * Return: The address of the node where the loop starts,
+ * or NULL if there is no loop.
+ */
+listint_t *_find_listint_loop(listint_t *head)
+{
+	listint_t *cur, *faster;
+
+	if (!head)
+		return (NULL);
+
+	cur = head;
+	faster = head;
+	while (cur && faster && (faster->next))
+	{
+		cur = cur->next;
+		faster = faster->next->next;
+		if (faster == cur)
+		{
+			cur = head;
+			while (cur != faster)
+			{
+				cur = cur->next;
+				faster = faster->next;
+			}
+			return (cur);
+		}
+	}
+	return (NULL);
 }
