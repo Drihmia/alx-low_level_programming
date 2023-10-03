1b548280ecf7b3498922acd942735545f2623901
Drihmia <drihmia.redouane@gmail.com>
2023-10-03 02:24:07 +0100

    save

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <args.h>
+
+/**
+ * main - take two aeguments, and copy 1st into the 2nd one.
+ * @ac: counts parameter passed to the program.
+ * @ag: arguments.
+ * Return: 0 if succeeded.
+ */
+
+int main(int ac, char **ag)
+{
+	if (ac != 3)
+	{
+		fprintf(stderr, "Usage: %s %s %s\n", ag[0], ag[1], ag[2]);
+		exit(97);
+	}
+}
