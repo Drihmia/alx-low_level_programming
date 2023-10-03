04293fc2741a05c484a9c53ac002f141f182c90a
Drihmia <drihmia.redouane@gmail.com>
2023-10-03 23:47:37 +0100

    separated the struct and definition in main.h

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,23 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 
-struct ElfHeader {
-	unsigned char e_ident[16];
-	unsigned short e_type;
-	unsigned short e_machine;
-	unsigned long e_version;
-	unsigned char e_osabi;
-	unsigned char e_abiversion;
-	unsigned char e_pad[7];
-	unsigned short e_type2;
-	unsigned long e_entry;
-};
 
-typedef struct ElfHeader ElfHeader;
-
-void displayElfHeaderInfo(const ElfHeader *header) {
+void displayElfHeaderInfo(const ElfHeader *header)
+{
 	printf("Magic:   0x7FELF\n");
 	printf("Class:   %s\n", header->e_ident[4] == 1 ? "32-bit" : "64-bit");
 	printf("Data:    %s\n", header->e_ident[5] == 1 ? "Little-endian" : "Big-endian");
@@ -28,42 +13,43 @@ void displayElfHeaderInfo(const ElfHeader *header) {
 	printf("Entry point address: 0x%lx\n", header->e_entry);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	const char *elf_filename;
 	int fd;
 	ElfHeader header;
 	ssize_t bytes_read;
 
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
 		exit(98);
 	}
 
 	elf_filename = argv[1];
 	fd = open(elf_filename, O_RDONLY);
-
-	if (fd == -1) {
+	if (fd == -1)
+	{
 		perror("Error opening file");
 		exit(98);
 	}
 
 	bytes_read = read(fd, &header, sizeof(ElfHeader));
-
-	if (bytes_read == -1) {
+	if (bytes_read == -1)
+	{
 		perror("Error reading file");
 		close(fd);
 		exit(98);
 	}
-
 	if (header.e_ident[0] != 0x7F ||
 			header.e_ident[1] != 'E' ||
 			header.e_ident[2] != 'L' ||
-			header.e_ident[3] != 'F') {
+			header.e_ident[3] != 'F')
+	{
 		fprintf(stderr, "Not a valid ELF file: %s\n", elf_filename);
 		close(fd);
 		exit(98);
 	}
-
 	displayElfHeaderInfo(&header);
 
 	close(fd);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+/**
+ * struct ElfHeader - ELF header structure
+ * @e_ident: ELF identification
+ * @e_type: Object file type
+ * @e_machine: Target architecture
+ * @e_version: Object file version
+ * @e_osabi: OS/ABI identification
+ * @e_abiversion: ABI version
+ * @e_pad: Padding
+ * @e_type2: Additional type field (unused)
+ * @e_entry: Entry point virtual address
+ *
+ * Description: Structure representing the ELF header of an ELF file.
+ */
+
+struct ElfHeader
+{
+	unsigned char e_ident[16];
+	unsigned short e_type;
+	unsigned short e_machine;
+	unsigned long e_version;
+	unsigned char e_osabi;
+	unsigned char e_abiversion;
+	unsigned char e_pad[7];
+	unsigned short e_type2;
+	unsigned long e_entry;
+};
+void displayElfHeaderInfo(const struct ElfHeader *header);
+
+
+typedef struct ElfHeader ElfHeader;
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
