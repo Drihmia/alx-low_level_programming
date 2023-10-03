b1b6788e0f75da11d8766d9b998a8922bae013b4
Drihmia <drihmia.redouane@gmail.com>
2023-10-03 12:47:38 +0100

    fixed a typo

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,33 +9,33 @@
 
 int main(int ac, char **ag)
 {
-	int fd_to = 0, fhandls_fr = 0;
+	int fd_to = 0, fd_fro = 0;
 	char buf[1024];
 	ssize_t ret_read = 0, ret_write = 0;
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s %s %s\n", ag[0], ag[1], ag[2]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
 	if (ag[1] == NULL || ag[2] == NULL)
 		return (-1);
-	fhandls_fr = open(ag[1], O_RDONLY);
-	if (fhandls_fr == -1)
+	fd_fro = open(ag[1], O_RDONLY);
+	if (fd_fro == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", ag[1]), exit(98);
 	fd_to = open(ag[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", ag[2]), exit(99);
-		if (close(fhandls_fr) == -1)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100); }
-	while ((ret_read = read(fhandls_fr, buf, sizeof(buf))) > 0)
+		if (close(fd_fro) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_fro), exit(100); }
+	while ((ret_read = read(fd_fro, buf, sizeof(buf))) > 0)
 	{
 		ret_write = write(fd_to, buf, ret_read);
 		if (ret_write == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", ag[2]), exit(99);
-			if (close(fhandls_fr) == -1)
-				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+			if (close(fd_fro) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_fro), exit(100);
 			if (close(fd_to) == -1)
 				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 		}
@@ -43,8 +43,8 @@ int main(int ac, char **ag)
 	if (ret_read == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ag[1]), exit(98);
 
-	if (close(fhandls_fr) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fhandls_fr), exit(100);
+	if (close(fd_fro) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_fro), exit(100);
 
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
