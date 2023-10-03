5eded9872f0a694d26bb82f764d4aaaf29fa15bd
Drihmia <drihmia.redouane@gmail.com>
2023-10-03 11:55:22 +0100

    d

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -34,7 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		}
 		buf[ret_read] = '\0';
 
-		ret_write = write(STDOUT_FILENO, buf, ret_read);
+		ret_write = write(STDOUT_FILENO, buf, strlen(buf));
 		if (ret_write == -1)
 		{
 			close(fhandl);
