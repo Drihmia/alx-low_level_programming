61c06a9ec0d599827662aa8d58fd1dafc6a3d3eb
Drihmia <drihmia.redouane@gmail.com>
2023-10-03 03:32:50 +0100

    save 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,9 +9,33 @@
 
 int main(int ac, char **ag)
 {
+	int fhandl_to = 0, fhandls_fr = 0;
+	char buf[1024];
+	ssize_t ret_read = 0, ret_write = 0;
+
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: %s %s %s\n", ag[0], ag[1], ag[2]);
 		exit(97);
 	}
+	if (ag[1] == NULL || ag[2] == NULL)
+		return (-1);
+	fhandls_fr = open(ag[1], O_RDONLY);
+	if (fhandls_fr == -1)
+		dprintf(2, "Error: Can't read from %s\n", ag[1]), exit(98);
+	fhandl_to = open(ag[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (fhandl_to == -1)
+		dprintf(2, "Error: Can't write to  %s\n", ag[2]), exit(99);
+	while ((ret_read = read(fhandls_fr, buf, sizeof(buf))) > 0)
+	{
+		ret_write = write(fhandl_to, buf, ret_read);
+		if (ret_write == -1)
+			dprintf(2, "Error: Can't write to %s\n", ag[2]), exit(99);
+	}
+	if (ret_read == -1)
+		dprintf(2, "Error: Can't read from file %s\n", ag[1]), exit(98);
+	if (close(fhandls_fr) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fhandls_fr), exit(100);
+	if (close(fhandl_to) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fhandl_to), exit(100);
 }
