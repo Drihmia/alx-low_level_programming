182a5023d02f21211e82951be685db22067725bd
Drihmia <drihmia.redouane@gmail.com>
2023-10-03 12:32:26 +0100

    help from vector

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,28 +9,36 @@
 
 int main(int ac, char **ag)
 {
-	int fhandl_to = 0, fhandls_fr = 0;
+	int fd_to = 0, fhandls_fr = 0;
 	char buf[1024];
 	ssize_t ret_read = 0, ret_write = 0;
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s %s %s\n", ag[0], ag[1], ag[2]);
-		exit(97);
-	}
+		exit(97); }
 	if (ag[1] == NULL || ag[2] == NULL)
 		return (-1);
 	fhandls_fr = open(ag[1], O_RDONLY);
 	if (fhandls_fr == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", ag[1]), exit(98);
-	fhandl_to = open(ag[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	if (fhandl_to == -1)
+	fd_to = open(ag[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", ag[2]), exit(99);
+		if (close(fhandls_fr) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100); }
 	while ((ret_read = read(fhandls_fr, buf, sizeof(buf))) > 0)
 	{
-		ret_write = write(fhandl_to, buf, ret_read);
+		ret_write = write(fd_to, buf, ret_read);
 		if (ret_write == -1)
+		{
 			dprintf(2, "Error: Can't write to %s\n", ag[2]), exit(99);
+			if (close(fhandls_fr) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+			if (close(fd_to) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+		}
 	}
 	if (ret_read == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ag[1]), exit(98);
@@ -38,7 +46,7 @@ int main(int ac, char **ag)
 	if (close(fhandls_fr) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fhandls_fr), exit(100);
 
-	if (close(fhandl_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fhandl_to), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
