bc7c877f3300903b37e1b4d7acf9545b99912186
Drrihmia <drihmia.redouane@gmail.com>
2023-11-10 17:43:49 -0800

    Write a keygen for crackme5.  link is : https://github.com/alx-tools/0x17.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,43 +1,167 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
+
+void *init_proc(void);
+int64_t sub_400520(void);
+int64_t f1(u_int8_t a1);
+int64_t f2(int64_t a1, int a2);
+int64_t f3(int64_t a1, int a2);
+int64_t f4(char *a1, int a2);
+int64_t f5(int64_t a1, int a2);
+int64_t f6(char a1);
+void segf(void);
+
+char v7[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 /**
- * main - entry point.
- * Return: None
+ * f1 - Performs bitwise XOR and AND operations on a1.
+ * @a1: An unsigned 8-bit integer.
+ *
+ * Return: Returns a 64-bit integer.
  */
-int main(void)
-{
-	int ref = 2772, sum = 0, holder, i = 0, diff;
-	char str[100];
 
-	srand(time(NULL));
-	while (sum != ref)
+int64_t f1(u_int8_t a1)
+{
+	return ((a1 ^ 0x3B) & 0x3F);
+}
+/**
+ * f2 - Iterates over a block of memory, sums up values,
+ * performs XOR and AND operations.
+ * @a1: A 64-bit integer.
+ * @a2: An integer.
+ *
+ * Return: Returns a 64-bit integer.
+ */
+int64_t f2(int64_t a1, int a2)
+{
+	u_int8_t v3 = 0;
+	int i;
+
+	for (i = 0; i < a2; ++i)
+		v3 += *((unsigned char *)(i + a1));
+	return ((v3 ^ 0x4F) & 0x3F);
+}
+/**
+ * f3 - Iterates over a block of memory, multiplies values,
+ * performs XOR and AND operations.
+ * @a1: A 64-bit integer.
+ * @a2: An integer.
+ *
+ * Return: Returns a 64-bit integer.
+ */
+int64_t f3(int64_t a1, int a2)
+{
+	char v3 = 1;
+	int i;
+
+	for (i = 0; i < a2; ++i)
+		v3 *= *((unsigned char *)(i + a1));
+	return (((u_int8_t)v3 ^ 0x55) & 0x3F);
+}
+/**
+ * f4 - Finds the maximum value in the character array, seeds the random
+ * number generator, and returns a random value.
+ * @a1: A pointer to a character array.
+ * @a2: An integer.
+ *
+ * Return: Returns a 64-bit integer.
+ */
+int64_t f4(char *a1, int a2)
+{
+	int v3;
+	int i;
+
+	v3 = *a1;
+	for (i = 0; i < a2; ++i)
 	{
-		holder = rand() % 128;
-		if (holder >= 44 && holder <= 126)
-		{
-			if (sum < ref)
-			{
-				str[i] = holder, i++;
-				sum += holder;
-				diff = ref - sum;
-				if ((diff) % 44 == 0 || (diff) / 44 <= 4)
-				{
-				while (sum < ref)
-				{
-					str[i] = 44;
-					sum += 44;
-					i++;
-					if (sum > ref)
-						break;
-				}
-				}
-			}
-		}
+		if (a1[i] > v3)
+			v3 = a1[i];
 	}
-	str[i] = '\0';
-	printf("%s", str);
+	srand(v3 ^ 0xE);
+	return (rand() & 0x3F);
+}
+/**
+ * f5 - Iterates over a block of memory, calculates the sum of squares,
+ * performs XOR and AND operations.
+ * @a1: A 64-bit integer.
+ * @a2: An integer.
+ *
+ * Return: Returns a 64-bit integer.
+ */
+
+int64_t f5(int64_t a1, int a2)
+{
+	u_int8_t v3 = 0;
+	int i;
+
+	for (i = 0; i < a2; ++i)
+		v3 += *((unsigned char *)(i + a1)) * *((unsigned char *)(i + a1));
+	return ((v3 ^ 0xEF) & 0x3F);
+}
+/**
+ * f6 - Generates a random value based on the ASCII
+ * value of the input character
+ * @a1: A character.
+ *
+ * Return: Returns a 64-bit integer.
+ */
+int64_t f6(char a1)
+{
+	u_int8_t v2 = 0;
+	int i;
+
+	for (i = 0; a1 > i; ++i)
+		v2 = rand();
+	return ((v2 ^ 0xE5) & 0x3F);
+}
+/**
+ * generate_key - Generates a key based on the provided username.
+ * @v5: A pointer to a string representing the username.
+ *
+ * Return: Returns a 64-bit integer representing the generated key.
+ */
+char *generate_key(char *v5)
+{
+	char *key = malloc(7);
+
+	key[0] = v7[(int)f1(strlen(v5))];
+	key[1] = v7[(int)f2((int64_t)v5, strlen(v5))];
+	key[2] = v7[(int)f3((int64_t)v5, strlen(v5))];
+	key[3] = v7[(int)f4(v5, strlen(v5))];
+	key[4] = v7[(int)f5((int64_t)v5, strlen(v5))];
+	key[5] = v7[(int)f6(*v5)];
+
+	return (key);
+}
+/**
+ * main - The main entry point of the program.
+ * @argc: An integer representing the number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: Returns an integer.
+ */
+int main(int argc, const char **argv)
+{
+	char *v5;
+	char *key;
+
+	if (argc != 2)
+	{
+		printf("Usage: %s username\n", argv[0]);
+		return (1);
+	}
+
+
+	v5 = (char *)argv[1];
+
+	key = generate_key(v5);
+
+	printf("%s\n", key);
+
+	free(key);
+
 	return (0);
 }
+
 
