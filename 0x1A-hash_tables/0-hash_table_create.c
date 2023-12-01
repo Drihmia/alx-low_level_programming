151931ec449d823b75d760b8494e9a96892b73e9
Drihmia <drihmia.redouane@gmail.com>
2023-12-01 01:44:48 +0100

    iterating until size -1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,17 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *h_table = NULL;
 	uli i;
 
-
-
-
 	h_table = malloc(sizeof(hash_table_t));
 	if (!h_table)
 		return (NULL);
 	h_table->size = size;
-	h_table->array = malloc(sizeof(hash_node_t *) * h_table->size);
+	h_table->array = malloc(sizeof(char *) * h_table->size);
 	if (!(h_table->array))
 		return (NULL);
-	for (i = 0; i < h_table->size - 1; i++)
+	for (i = 0; i < h_table->size; i++)
 	{
 		h_table->array[i] = NULL;
 	}
