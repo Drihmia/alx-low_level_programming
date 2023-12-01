717c24376241da7beecf1ee97d3fadf5da7e2616
Drihmia <drihmia.redouane@gmail.com>
2023-12-01 03:54:13 +0100

    a function that delete a hash table and free memeroy

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - a function that deletes a hash table.
+ * @ht: the hash table
+ * Return: None
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	uli i;
+	hash_node_t *head = NULL, *tmp = NULL;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		head = ht->array[i];
+
+		while (tmp)
+		{
+			head = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = head;
+		}
+	}
+	free(ht->array);
+}
