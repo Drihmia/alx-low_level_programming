0038979be7dbcc6eaad04666e8917760fb9aea05
Drihmia <drihmia.redouane@gmail.com>
2023-12-01 01:51:12 +0100

    if index is negative, return 0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,6 +18,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key)
 		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index < 0)
+		return (0);
+
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
@@ -29,7 +33,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	node->next = NULL;
 
-	index = key_index((const unsigned char *)key, ht->size);
 	head = ht->array[index];
 	if (ht->array && head)
 	{
