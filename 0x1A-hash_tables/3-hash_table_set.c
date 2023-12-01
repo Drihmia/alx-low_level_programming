1959fef368d8cd6f7263f2bcbd4687a90887cca9
Drihmia <drihmia.redouane@gmail.com>
2023-12-01 21:06:34 +0100

    check if also 2nd char is null for the key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL, *head = NULL, *tmp = NULL;
 	uli index = 0;
 
-	if (!ht || !key || !(*(key + 0)))
+	if (!ht || !key || !(*(key + 0) || !(*(key + 1))))
 		return (0);
 	if (ht->size == 0)
 		return (0);
