05149c368556c158c85c0d16a00922696dc3a52d
Drihmia <drihmia.redouane@gmail.com>
2023-12-02 02:49:33 +0100

    update2

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -150,7 +150,7 @@ void shash_table_print(const shash_table_t *ht)
  * appear in the array of hash table (Order: array, list).
  * If ht is NULL, don’t print anything
  */
-void shash_table_print(const shash_table_t *ht)
+void shash_table_print_rev(const shash_table_t *ht)
 {
 	(void) ht;
 }
