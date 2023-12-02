09814ee9b847cefd9156db865652e369986a79dc
Drihmia <drihmia.redouane@gmail.com>
2023-12-02 01:01:45 +0100

    fixed a typo

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -44,7 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				tmp_val = strdup(value);
 				if (!tmp_val)
 					return (0);
-				free(tmp->value), tmp->value = strdup(value);
+				free(tmp->value), tmp->value = tmp_val;
 				return (1);
 			}
 			tmp = tmp->next; }
