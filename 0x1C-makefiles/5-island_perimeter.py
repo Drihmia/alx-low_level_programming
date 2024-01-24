d3d7ee5ddb50774ba0df379bfce6e661845a8471
Drihmia <drihmia.redouane@gmail.com>
2024-01-24 19:05:30 +0100

    handled the cases where there is only one line or an element has been repeated only once

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,13 +9,18 @@ def island_perimeter(grid):
     """ a funtion that returns the perimeter of the
     island described in grid
     """
-
+    if not grid:
+        return 0
     start_x, start_y = [], []
     for i, row in enumerate(grid):
         for j, cell in enumerate(row):
             if cell == 1:
                 start_x.append(j)
                 start_y.append(i)
+    most_common_x = most_common_element(start_x)
+    most_common_y = most_common_element(start_y)
+    if not most_common_x or not most_common_y:
+        return 0
     return (most_common_element(start_x) + most_common_element(start_y)) * 2
 
 
@@ -32,4 +37,6 @@ def most_common_element(lst):
         count = lst.count(item)
         if count > max_count:
             max_count = count
+    if max_count == 1:
+        max_count = 0
     return max_count
