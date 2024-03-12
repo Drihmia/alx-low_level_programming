f377bff0c0646ea4f53aed7248c4084ff3a5e5f5
Drihmia <drihmia.redouane@gmail.com>
2024-03-12 13:34:01 +0000

    adjustement

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,7 @@ int jump_search(int *array, size_t size, int value)
 	jump = sqrt(size);
 	for (i = 0; i < size; i += jump)
 	{
-		if (i + jump > size)
+		if (i + jump >= size)
 			limit = size - 1;
 		else
 			limit = i + jump;
